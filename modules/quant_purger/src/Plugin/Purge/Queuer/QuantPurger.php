3821ce72f64e25e6dec35746b7df70b1f25c925d
Roman Barbun <romanbarbun@gmail.com>
2025-09-09 01:46:40 +1000

    Upgrade Quant.

--- modules/quant_purger/src/Plugin/Purge/Queuer/QuantPurger.php
+++ modules/quant_purger/src/Plugin/Purge/Queuer/QuantPurger.php
@@ -4,14 +4,19 @@ namespace Drupal\quant_purger\Plugin\Purge\Queuer;
 
 use Drupal\Core\Cache\CacheTagsInvalidatorInterface;
 use Drupal\quant\Plugin\QueueItem\RouteItem;
-use Symfony\Component\DependencyInjection\ContainerAwareInterface;
-use Symfony\Component\DependencyInjection\ContainerAwareTrait;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Queues URLs with Quant when Drupal invalidates cache tags.
  */
-class QuantPurger implements CacheTagsInvalidatorInterface, ContainerAwareInterface {
-  use ContainerAwareTrait;
+class QuantPurger implements CacheTagsInvalidatorInterface {
+
+  /**
+   * The service container.
+   *
+   * @var \Symfony\Component\DependencyInjection\ContainerInterface
+   */
+  protected $container;
 
   /**
    * A list of tags that have already been invalidated in this request.
@@ -54,6 +59,16 @@ class QuantPurger implements CacheTagsInvalidatorInterface, ContainerAwareInterf
    * @var null|\Drupal\Core\Queue\QueueInterface
    */
   protected $quantSeedQueue;
+
+  /**
+   * Constructs a QuantPurger object.
+   *
+   * @param \Symfony\Component\DependencyInjection\ContainerInterface $container
+   *   The service container.
+   */
+  public function __construct(ContainerInterface $container) {
+    $this->container = $container;
+  }
 
   /**
    * Initialize the invalidation factory and queue service.
